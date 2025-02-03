65471ae4e2bb6f9e5d348d3158a12b2acc0df743
Rub21 <rub2106@gmail.com>
2025-02-03 08:09:38 -0500

    Updates

---
+++ assets/index.ts
@@ -0,0 +1,76 @@
+import logo from "./images/LogoBig.png";
+import profileImg from "./images/me.jpg";
+import profileImgCircle from "./images/me.jpg";
+
+import FaceDetection from "./images/ProjectPictures/FaceDetection.png";
+import Ecommerce from "./images/ProjectPictures/Ecommerce.webp";
+import MissionControl from "./images/ProjectPictures/Mission-Control.jpg";
+import SpotifyClone from "./images/ProjectPictures/SpotifyClone.webp"
+
+import JavaScriptL from "./images/Skills/JavaScriptL.png";
+import TypeScript from "./images/Skills/TypeScript.png";
+import Python from "./images/Skills/python.png";
+import CSS from "./images/Skills/CSS.png";
+import HTML from "./images/Skills/HTML.png";
+import ReactL from "./images/Skills/React.png";
+import Next from "./images/Skills/Next2.png";
+import Redux from "./images/Skills/Redux.png";
+import NodeJS from "./images/Skills/NodeJS.png";
+import Express from "./images/Skills/Express.png";
+import PostgreSQL from "./images/Skills/PostgreSQL.png";
+import MongoDB from "./images/Skills/MongoDB.png";
+import GraphQL from "./images/Skills/GraphQL.png";
+
+import Mapbox from "./images/Skills/mapbox.png";
+import Openlayer from "./images/Skills/openlayer.png";
+import Leafleat from "./images/Skills/leafleat.png";
+import Java from "./images/Skills/Java.png";
+import Hapi from "./images/Skills/hapi.svg";
+
+import Github from "./images/Skills/Github.webp";
+import Docker from "./images/Skills/Docker.png";
+import Kubernetes from "./images/Skills/Kubernetes.png";
+
+import AWS from "./images/Skills/AWS.png";
+import GCP from "./images/Skills/GCP.png";
+
+
+import QGis from "./images/Skills/QGis.png";
+import ArcGIS from "./images/Skills/ArcGIS.png";
+import OSM from "./images/Skills/OSM.png";
+
+
+export {
+    OSM,
+    QGis,
+    ArcGIS,
+    AWS,
+    GCP,
+    Github,
+    Docker,
+    Kubernetes,
+     CSS,
+     HTML,
+     ReactL,
+     Mapbox,
+     Next,
+     Leafleat,
+     Openlayer,
+     Redux,
+     NodeJS,
+     Express,
+     PostgreSQL,
+     MongoDB,
+     GraphQL,
+     JavaScriptL,
+     TypeScript,
+     Python,
+     logo,
+     profileImg,
+     profileImgCircle,
+     FaceDetection,
+     Ecommerce,
+     MissionControl,
+     SpotifyClone,
+     Java,
+     Hapi };
